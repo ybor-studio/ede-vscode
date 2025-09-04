3e0a50447277fcc3b3a4d9d2f19349a0102ae64c
christiannuss-ybor <christian@ybor.ai>
2025-09-04 17:55:00 +0000

    scaffolding for tunnel providers

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,24 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { log, NAME } from "./log";
+import { activatePorts } from "./ports";
 
 export async function activate(context: vscode.ExtensionContext) {
-	console.log('Activating EDE VSCode extension...');
-	await vscode.commands.executeCommand('setContext', 'forwardedPortsViewEnabled', true);
+  log(`Activating ${NAME} extension...`);
+  await activatePorts(context);
 
-	// DEVNOTE: Update package.json for command registrations
-	let disposable = vscode.commands.registerCommand('ede-vscode.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from EDE VSCode!');
-	});
+  // DEVNOTE: Update package.json for command registrations
+  let disposable = vscode.commands.registerCommand(
+    "ede-vscode.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from EDE VSCode!");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+  log(`Activated ${NAME} extension!`);
 }
 
 export function deactivate() {
-	console.log('Deactivating EDE VSCode extension...');
-	// Clean up resources if necessary
+  // console.log("Deactivating EDE VSCode extension...");
+  // Clean up resources if necessary
 }
---
+++ src/ports.ts
@@ -0,0 +1,76 @@
+import * as vscode from "vscode";
+import { log } from "./log";
+import { resolve } from "path";
+import { rejects } from "assert";
+
+export async function activatePorts(context: vscode.ExtensionContext) {
+  log("Activating Ports View...");
+  await vscode.commands.executeCommand(
+    "setContext",
+    "forwardedPortsViewEnabled",
+    true
+  );
+
+  context.subscriptions.push(
+    await vscode.workspace.registerTunnelProvider(new TunnelProvider(), {
+      environmentTunnels: [new Tunnel()],
+      tunnelFeatures: {
+        elevation: false,
+        privacyOptions: [
+          {
+            id: "private",
+            label: "Private",
+            themeIcon: "globe",
+          },
+        ],
+      },
+    })
+  );
+}
+
+class TunnelProvider implements vscode.TunnelProvider {
+  constructor() {
+    log("!!! TunnelProvider constructor");
+  }
+
+  provideTunnel(
+    tunnelOptions: vscode.TunnelOptions,
+    tunnelCreationOptions: vscode.TunnelCreationOptions,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.Tunnel> {
+    log("!!! TunnelProvider.provideTunnel", {
+      tunnelOptions,
+      tunnelCreationOptions,
+      token,
+    });
+    return this.build(tunnelOptions, tunnelCreationOptions, token);
+  }
+
+  private async build(
+    tunnelOptions: vscode.TunnelOptions,
+    tunnelCreationOptions: vscode.TunnelCreationOptions,
+    token: vscode.CancellationToken
+  ): Promise<Tunnel> {
+    return new Tunnel();
+  }
+}
+
+class Tunnel implements vscode.Tunnel {
+  remoteAddress: { port: number; host: string };
+  localAddress: string | { port: number; host: string };
+  public?: boolean | undefined;
+  privacy?: string | undefined;
+  protocol?: string | undefined;
+  onDidDispose: vscode.Event<void>;
+
+  constructor() {
+    log("!!! Tunnel Constructor");
+    this.remoteAddress = { host: "todo", port: 443 };
+    this.localAddress = { host: "localhost", port: 3000 };
+    this.onDidDispose = () => new vscode.Disposable(() => this.dispose());
+  }
+
+  dispose(): void {
+    log("!!! Tunnel Dispose");
+  }
+}
